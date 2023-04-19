44442fbbaefae09df3fae5645f794a3953c59fd6
Elmurodjon01 <abdulajonov01@gmail.com>
2023-04-19 23:36:16 +0900

    starting project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app_with_riverpod/src/routing.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
